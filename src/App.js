e416e1cd4da9c7b60d14cc643799f0b0f6b15485
ielbusmente <ieljbusmente@gmail.com>
2022-04-21 21:40:07 +0800

    label class and button func login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [ signup, setSignUp] = useState(true)
   return (
     <div class="form">
+        
+      <ul class="tab-group">
+        <li class={`tab ${signup ? 'active':''}`}><a onClick={() => setSignUp(true)}>Sign Up</a></li>
+        <li class={`tab ${!signup ? 'active':''}`}><a onClick={() => setSignUp(false)}>Log In</a></li>
+      </ul>
       <div class="tab-content">
         {signup  ?<Registration />:<Login />}
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [studentNumb, setstudentNumb] = useState(``) 
+  const [password, setpassword] = useState(``) 
   return (
     <div>
      <div id="login">   
           <h1>Login </h1>
           
-          <form action="/" method="post">
+          <form 
+          // action="/" method="post"
+          >
           
             <div class="field-wrap">
-            <label>
+            <label className={`${studentNumb!=='' ? `active highlight`:``}`}>
                Student Number<span class="req">*</span>
             </label>
-            <input type="text"required autocomplete="off"/>
+            <input type="text"required autocomplete="off" onChange={(e)=>setstudentNumb(e.target.value)} value={studentNumb}/>
           </div>
           
           <div class="field-wrap">
-            <label>
+            <label className={`${password!=='' ? `active highlight`:``}`}>
               Password<span class="req">*</span>
             </label>
-            <input type="password"required autocomplete="off"/>
+            <input type="password"required autocomplete="off" onChange={(e)=>setpassword(e.target.value)} value={password}/>
           </div>
           
-          <p class="forgot"><a href="#">Forgot Password?</a></p>
+          {/* <p class="forgot"><a href="#">Forgot Password?</a></p> */}
           
-          <button class="button button-block">Log In</button>
+          <button class="button button-block" onClick={()=>{
+           studentNumb!=='' && password!=='' &&  alert('Welcome');
+          }}>
+            Log In
+          </button>
+          <button type='button' class="button button-block" onClick={()=>{
+            setstudentNumb(``)
+            setpassword(``)
+          }}>
+            Cancel
+          </button>
           
           </form>
 
